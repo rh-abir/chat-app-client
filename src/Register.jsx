a2f2b5a48dff73d7a4d158d551110922906608c0
rh-abir <rhabir.co@gmail.com>
2023-08-23 23:52:40 +0600

    make register form

---
+++ src/Register.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = (e) => {
+    e.preventDefault();
+    
+  };
+
+  return (
+    <div className="bg-blue-50 h-screen flex justify-center items-center">
+      <form onSubmit={register} className="w-64 mb-12">
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          placeholder="username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="password"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
